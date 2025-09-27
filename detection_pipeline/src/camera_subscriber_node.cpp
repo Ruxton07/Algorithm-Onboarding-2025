8645243dd12b0b97b0b9384007759e033b5c5a3f
Ruxton07 <rt.kellar@gmail.com>
2025-09-27 13:17:17 -0400

    image processing with cv bridge

--- detection_pipeline/src/camera_subscriber_node.cpp
+++ detection_pipeline/src/camera_subscriber_node.cpp
@@ -4,9 +4,18 @@
 CameraSubscriberNode::CameraSubscriberNode()
 : Node("camera_subscriber_node")
 {
-    subscriber_ = this->create_subscription<sensor_msgs::msg::Image>("camera/image_raw", rclcpp::SensorDataQoS);
+    subscription_ = this->create_subscription<sensor_msgs::msg::Image>(
 
+        
 }
+
+// call back to read from topic, that will give you your matrix
+void CameraSubscriberNode::image_callback(const sensor_msgs::msg::Image::SharedPtr msg)
+{
+    cv::Mat frame = cv_bridge::toCvShare(msg, "bgr8")->image;
+    RCLCPP_INFO(this->get_logger(), "i got an image with size: %dx%d", frame.cols, frame.rows);
+}
+
 
 int main(int argc, char *argv[])
 {
